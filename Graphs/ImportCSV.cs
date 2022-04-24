a88113a61634b1b528dd8791a8bad35cc79e52eb
tomczakmarcel <tomczakmarcel592@gmail.com>
2022-04-24 22:09:45 +0200

    SolarWind models and methods added, but we need to upgrade calculation for solar energy according to test case We will also need to take care of energy production window data

--- Graphs/ImportCSV.cs
+++ Graphs/ImportCSV.cs
@@ -1,5 +1,6 @@
 ﻿using LumenWorks.Framework.IO.Csv;
 using System.Data;
+using System.Globalization;
 
 namespace AplikacjaSmartGrid.Graphs
 {
@@ -12,7 +13,7 @@ namespace AplikacjaSmartGrid.Graphs
         private static DataTable LoadCSV(string csvPath = @"D:\PULPIT\MTomczak_TEST.csv")
         {
             var csvTable = new DataTable();
-            using (var csvReader = new CsvReader(new StreamReader(System.IO.File.OpenRead(@"D:\PULPIT\MTomczak_TEST.csv")), true, ';'))
+            using (var csvReader = new CsvReader(new StreamReader(System.IO.File.OpenRead(csvPath)), true, ';'))
             {
                 csvTable.Load(csvReader);
             }
@@ -43,6 +44,36 @@ namespace AplikacjaSmartGrid.Graphs
             foreach (var element in test3)
             {
                 searchParameters2.Add(new UserUsageModel { PPE = element.PPE, DATACZAS = element.DATACZAS, ZUZYCIE = element.ZUZYCIE});
+            }
+
+            return searchParameters2;
+        }
+
+        public static List<SolarWindProductionDataModel> ReturnListWindSolar()
+        {
+            double efficiency = 0.18;
+            double maxPower = 15;
+            var csvTable = LoadCSV(@"D:\PULPIT\EnergyWindProduction2021\AllDataEnergyWindProd.csv");
+            List<SolarWindProductionDataModel> searchParameters = new List<SolarWindProductionDataModel>();
+            List<SolarWindProductionDataModel> searchParameters2 = new List<SolarWindProductionDataModel>();
+
+            for (int i = 0; i < csvTable.Rows.Count; i++)
+            {
+                searchParameters.Add(new SolarWindProductionDataModel { DateOfProduction = DateOnly.Parse(Convert.ToString(csvTable.Rows[i][0])), SolarProduction = Convert.ToDouble(csvTable.Rows[i][3], CultureInfo.InvariantCulture) * efficiency * maxPower, WindProduction = Convert.ToDouble(csvTable.Rows[i][2], CultureInfo.InvariantCulture) });
+            }
+
+            var groupedValuesForDay = searchParameters
+                .GroupBy(x => x.DateOfProduction)
+                .Select(group => new
+                {
+                    DateOfProduction = group.Key,
+                    SolarProduction = group.Select(SolarWindProductionDataModel => SolarWindProductionDataModel.SolarProduction).Sum(),
+                    WindProduction = group.Select(SolarWindProductionDataModel => SolarWindProductionDataModel.WindProduction).Sum()
+                });
+
+            foreach (var element in groupedValuesForDay)
+            {
+                searchParameters2.Add(new SolarWindProductionDataModel { DateOfProduction = DateOnly.Parse(Convert.ToString(element.DateOfProduction)), WindProduction = element.WindProduction, SolarProduction = element.SolarProduction });
             }
 
             return searchParameters2;
---
+++ Graphs/SolarWindProductionDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AplikacjaSmartGrid.Graphs
+{
+    public class SolarWindProductionDataModel
+    {
+        public DateOnly DateOfProduction { get; set; }
+        public double SolarProduction { get; set; }
+        public double WindProduction { get; set; }
+    }
+}
