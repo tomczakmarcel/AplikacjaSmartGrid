a78acede4044ebdb7c9a6504e4c820bac2147791
tomczakmarcel <tomczakmarcel592@gmail.com>
2022-06-19 21:52:26 +0200

    Graph added, own site added

---
+++ Graphs/Model/WindProductionDataModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AplikacjaSmartGrid.Graphs.Model
+{
+    public class WindProductionDataModel
+    {
+        public DateTime DateOfProduction { get; set; }
+        public double WindProduction { get; set; }
+    }
+}
