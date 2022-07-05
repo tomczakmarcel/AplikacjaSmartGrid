d2bdf565244a3507842ab54b69367e11c1d4d911
tomczakmarcel <tomczakmarcel592@gmail.com>
2022-07-05 12:54:03 +0200

    Energy balance graph added

---
+++ Graphs/Model/EnergyBalanceModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AplikacjaSmartGrid.Graphs.Model
+{
+    public class EnergyBalanceModel
+    {
+        public DateTime DateOfProduction { get; set; }
+        public double EnergyBalance { get; set; }
+    }
+}
